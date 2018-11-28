6058f043a5094477c0d2f9497143ff30653be310
SangJinny <sangjinny28@gmail.com>
2018-11-28 22:29:54 +0900

    first commit

---
+++ src/EX_Expression_002.kt
@@ -0,0 +1,3 @@
+fun main (agrs:Array<String>): Unit {
+    53+62-126
+}
---
+++ src/EX_LiteralDataType_004.kt
@@ -0,0 +1,4 @@
+fun main (args:Array<String>) :Unit {
+    var variable = 10+12-5 // var variable:Int= ~~와 같음.
+    println(variable)
+}
